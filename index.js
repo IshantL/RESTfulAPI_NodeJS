66a788a4ee086b742822743ec3ad64c0485dcf1c
IshantL <ishantlambhate@gmail.com>
2018-10-24 09:20:04 +0000

    paesing HTTP methods,Query String,Headers

--- index.js
+++ index.js
@@ -8,20 +8,29 @@ var url = require('url');
 
 var server = http.createServer(function(req,res){
 
-    //get the url and parse it
+//get the url and parse it
 var parsedUrl=url.parse(req.url,true);
 
 //get the path
-
 var path = parsedUrl.pathname;
 var trimmedPath = path.replace(/\/+|\/+$/g,'');
+
+//Get the query string as an object 
+var queryString = parsedUrl.query;
+
+//Get the HTTP method
+var method= req.method.toLowerCase();
+
+//gets the headers as an object
+var headers= req.headers;
 
 //send the response
 res.end('Hello World');
 
 //log the request path
-console.log('request is recieved on path:'+trimmedPath);
-   
+console.log('request is recieved on path:'+trimmedPath+' with method:'+method+'with query param',queryString);
+//log for headers
+console.log('headers',headers);
 });
 //Start the server , and have it listen to port 3000
 
